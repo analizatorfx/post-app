45830277c8f51e66a46e6e16e57ac967cd9ef2a3
Marek <fx@marksoft.com.pl>
2021-01-31 10:10:06 +0100

    feat(app): AIO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { PostsListComponent } from './component/posts-list/posts-list.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PostsListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
